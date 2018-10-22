aadb5b5e12b09f37a2791af129f1d3d9f62b2c39
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-10-22 15:31:10 -0400

    Removed posix from resolve utils

--- src/utils/resolve.utils.js
+++ src/utils/resolve.utils.js
@@ -1,7 +1,7 @@
 const { VERSION, RESOURCE_CONFIG } = require('../config');
 const path = require('path');
 
-let base = path.posix.join(__dirname, '../');
+let base = path.join(__dirname, '../');
 
 let resolveFromVersion = (version = VERSION['3_0_1'], relative_path = '') => {
 	return path.posix.join(base, RESOURCE_CONFIG.resourceBase, version, relative_path);
