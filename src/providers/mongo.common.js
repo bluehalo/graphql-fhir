7ed6e74d20527e74721b08b03bdfc06e219047e6
Michael Booth <61957561+michaelrbooth@users.noreply.github.com>
2021-05-22 15:53:04 -0700

    add update and remove

--- src/providers/mongo.common.js
+++ src/providers/mongo.common.js
@@ -3,6 +3,8 @@ const logger = require('@asymmetrik/node-fhir-server-core').loggers.get();
 const {resolveSchema} = require('@asymmetrik/node-fhir-server-core');
 const {verifyHasValidScopes} = require('./security');
 const {getUuid} = require('./uid');
+const {applyPatch, compare} = require('fast-json-patch');
+const deepEqual = require('fast-deep-equal');
 const {buildDstu2SearchQuery, buildStu3SearchQuery, buildR4SearchQuery} = require('./query.builder');
 const errorUtils = require('../utils/error.utils');
 const moment = require('moment-timezone');
@@ -161,20 +163,148 @@ module.exports.searchById = async (args, context, resource_name, collection_name
 };
 
 /**
+ * does a FHIR Update (PUT)
+ * @param {string[]} args
+ * @param {IncomingMessage} req
+ * @param {string} resource_name
+ * @param {string} collection_name
+ */
+ module.exports.update = async (args, context, resource_name, collection_name) => {
+    const db = context.server.db;
+    const version = context.version;
+    const resource_incoming = args.resource;
+    verifyHasValidScopes(resource_name, 'write', resource_incoming.patient.resource); //TODO add scopes req.authInfo && req.authInfo.scope);
+    const id = args.id;
+
+    try {
+        let collection = db.collection(`${collection_name}_${version}`);
+        // Get current record
+        const data = await collection.findOne({id: id.toString()});
+        // create a resource with incoming data
+        let Resource = getResource(version, resource_name);
+        let cleaned;
+        let doc;
+
+        // check if resource was found in database or not
+        // noinspection JSUnresolvedVariable
+        if (data && data.meta) {
+            // found an existing resource
+            let foundResource = new Resource(data);
+
+            // use metadata of existing resource (overwrite any passed in metadata)
+            resource_incoming.meta = foundResource.meta;
+
+            // now create a patch between the document in db and the incoming document
+            //  this returns an array of patches
+            let patchContent = compare(data, resource_incoming);
+            // ignore any changes to _id since that's an internal field
+            patchContent = patchContent.filter(item => item.path !== '/_id');
+            // see if there are any changes
+            if (patchContent.length === 0) {
+                logInfo('No changes detected in updated resource');
+                return {
+                    id: id,
+                    created: false,
+                    resource_version: foundResource.meta.versionId,
+                };
+            }
+            // now apply the patches to the found resource
+            let patched_incoming_data = applyPatch(data, patchContent).newDocument;
+            let patched_resource_incoming = new Resource(patched_incoming_data);
+            let meta = foundResource.meta;
+            meta.versionId = `${parseInt(foundResource.meta.versionId) + 1}`;
+            meta.lastUpdated = moment.utc().format('YYYY-MM-DDTHH:mm:ssZ');
+            patched_resource_incoming.meta = meta;
+            cleaned = JSON.parse(JSON.stringify(patched_resource_incoming));
+            doc = Object.assign(cleaned, {_id: id});
+            check_fhir_mismatch(cleaned, patched_incoming_data);
+        } else {
+            // not found so insert
+            // create the metadata
+            let Meta = getMeta(version);
+            resource_incoming.meta = new Meta({
+                versionId: '1',
+                lastUpdated: moment.utc().format('YYYY-MM-DDTHH:mm:ssZ'),
+            });
+            cleaned = JSON.parse(JSON.stringify(resource_incoming));
+            doc = Object.assign(cleaned, {_id: id});
+        }
+
+        // Insert/update our resource record
+        // When using the $set operator, only the specified fields are updated
+        const res = await collection.findOneAndUpdate({id: id}, {$set: doc}, {upsert: true});
+        // save to history
+        let history_collection = db.collection(`${collection_name}_${version}_History`);
+        let history_resource = Object.assign(cleaned, {_id: id + cleaned.meta.versionId});
+        await history_collection.insertOne(history_resource);
+
+        return {
+            id: id,
+            created: res.lastErrorObject && !res.lastErrorObject.updatedExisting,
+            resource_version: doc.meta.versionId,
+        };
+    } catch (e) {
+        let error = errorUtils.internal(version, e.message);
+		return errorUtils.formatErrorForGraphQL(error);
+    }
+};
+
+/**
+ * does a FHIR Remove (DELETE)
+ * @param {string[]} args
+ * @param {IncomingMessage} req
+ * @param {string} resource_name
+ * @param {string} collection_name
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports.remove = async (args, context, resource_name, collection_name) => {
+    const db = context.server.db;
+    const version = context.version;
+    const id = args.id;
+    verifyHasValidScopes(resource_name, 'write', id)//;, req.authInfo && req.authInfo.scope);
+    let collection = db.collection(`${collection_name}_${version}`);
+    // Delete our resource record
+    let res;
+    try {
+        res = await collection.deleteOne({id: id});
+    } catch (e) {
+        let error = errorUtils.internal(version, e.message);
+		return errorUtils.formatErrorForGraphQL(error);
+    }
+    // delete history as well.  You can chose to save history.  Up to you
+    let history_collection = db.collection(`${collection_name}_${version}_History`);
+    try {
+        await history_collection.deleteMany({id: id});
+    } catch (e) {
+        let error = errorUtils.internal(version, e.message);
+		return errorUtils.formatErrorForGraphQL(error);
+    }
+    return {id: id};
+};
+
+/**
  * Gets class for the given resource_name and version
- * @param {string} base_version
+ * @param {string} version
  * @param {string} resource_name
  * @returns {function(?Object):Resource}
  */
- let getResource = (base_version, resource_name) => {
-    return resolveSchema(base_version, resource_name);
+ let getResource = (version, resource_name) => {
+    return resolveSchema(version, resource_name);
 };
 
 /**
  * Gets class for Meta
- * @param {string} base_version
+ * @param {string} version
  * @returns {function({Object}):Meta} Meta class
  */
- let getMeta = (base_version) => {
-    return resolveSchema(base_version, 'Meta');
+ let getMeta = (version) => {
+    return resolveSchema(version, 'Meta');
+};
+
+let check_fhir_mismatch = (cleaned, patched) => {
+    if (deepEqual(cleaned, patched) === false) {
+        let diff = compare(cleaned, patched);
+        logger.warn('Possible FHIR mismatch - ' + cleaned.resourceType + cleaned.id + ':' + cleaned.resourceType);
+        logger.warn(diff);
+    }
 };
