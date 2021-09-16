5d93e824ae98f5b00c4f57cda3ac774077b2b3b3
Luan <ltran@asymmetrik.com>
2021-09-16 10:59:55 -0400

    task: resolve graphql dep

--- src/utils/schema.utils.js
+++ src/utils/schema.utils.js
@@ -1,11 +1,11 @@
-const { coerceValue } = require('graphql/utilities');
+const { coerceInputValue } = require('graphql/utilities');
 
 /**
  * @name mapJsonToSchema
  * @summary Coerce JSON to schema values or return errors if value is incorrect
  */
 const mapJsonToSchema = (json, graphqlInputObject) =>
-	coerceValue(json, graphqlInputObject);
+	coerceInputValue(json, graphqlInputObject);
 
 module.exports = {
 	mapJsonToSchema,
